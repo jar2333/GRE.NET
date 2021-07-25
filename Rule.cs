ba3734115f558ea253d4d58d9d82b8eb4a463fc6
neonet1 <38730172+neonet1@users.noreply.github.com>
2021-07-25 15:48:51 -0400

    standardized Rule class and specified assumptions in README

--- Rule.cs
+++ Rule.cs
@@ -5,12 +5,23 @@ using QuikGraph;
 namespace GraphRewriteEngine
 {
     public class Rule {
-        public BidirectionalGraph<Node, LEdge> LHS;
-        public BidirectionalGraph<Node, LEdge> I;
-        public BidirectionalGraph<Node, LEdge> RHS;
+        public UndirectedGraph<Node, LEdge> LHS;
+        public UndirectedGraph<Node, LEdge> RHS;
+        public UndirectedGraph<Node, LEdge> I;
 
         public Morphism L;
         public Morphism R;
+
+        public Rule(UndirectedGraph<Node, LEdge> LHS, UndirectedGraph<Node, LEdge> RHS, 
+                    UndirectedGraph<Node, LEdge> I, Morphism L, Morphism R) {
+            this.LHS = LHS;
+            this.RHS = RHS;
+            this.I = I;
+            this.L = L;
+            this.R = R;
+
+            
+        }
 
 
 
